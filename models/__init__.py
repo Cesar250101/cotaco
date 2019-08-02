148e089f4deca4bdaadef1965a0fbb673665bbcd
Pedro Ulloa <pulloam@hotmail.com>
2019-08-02 15:41:41 -0400

    prueba cambiando nombre archivo

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import models, nomina,compras,contabilidad,inventario,productos,ventas,produccion,reportes
+from . import models, nomina,orden_compra,contabilidad,inventario,productos,ventas,produccion,reportes
